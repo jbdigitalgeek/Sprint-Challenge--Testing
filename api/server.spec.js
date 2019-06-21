fdfd032d2f00d88bd87c7575bdcf463cca4eb7fb
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-21 10:16:34 -0700

    debugged and refactored working mvp

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,71 +2,72 @@ const server = require("./server.js");
 const request = require("supertest");
 const db = require("../data/dbConfig.js");
 
-describe("Server tests", () => {
-  beforeEach(() => {
-    return db("games").truncate();
-  });
-
-  it("should set testing env", () => {
-    const env = process.env.DB_ENV;
-
-    expect(env).toBe("testing");
-  });
-});
-
-describe("GET /games", () => {
-  it("should return 200", async () => {
-    const res = await request(server).get("/games");
-    expect(res.status).toBe(200);
-  });
-
-  it("should return JSON", async () => {
-    const res = await request(server).get("/games");
-    expect(res.type).toBe("application/json");
-  });
-
-  it("should return an array even if there are no games", async () => {
-    const res = await request(server).get("/games");
-
-    expect(res.body).toEqual([]);
-  });
-});
-
-describe("POST /games", () => {
-  it("should return 422 if data is missing", async () => {
-    await db("games").insert({
-      title: "Pacman",
-      genre: "Arcade",
-      releaseYear: 1980
+describe('GET ("/games") ', () => {
+    it('returns status code 200', async () => {
+        let res = await request(server).get('/games');
+        expect(res.status).toBe(200);
     });
 
-    const res = await request(server).post("/games");
-
-    expect(res.status).toBe(422);
-  });
-  it("should return 200", async () => {
-    const info = [
-      {
-        title: "Pacman",
-        genre: "Arcade",
-        releaseYear: 1980
-      }
-    ];
-
-    await db("games").insert(info);
-
-    const res = await request(server).post("/games", info);
-
-    expect(res.status).toBe(200);
-  });
-  it("should return json", async () => {
-    await db("games").insert({
-      title: "Pacman",
-      genre: "Arcade",
-      releaseYear: 1980
+    it('returns JSON data', async () => {
+        const res = await request(server).get('/games');
+        expect(res.type).toBe('application/json'); 
     });
 
-    const res = await request(server).post("/games");
-    expect(res.type).toBe("application/json");
-  });
+
+    it('is functional', async () => {
+        const res = await request(server).get('/games');
+        expect(typeof res.body).toEqual('object');
+    });
 });
+
+
+ describe('GET ("/games")', () => {
+    it('returns status code 200', async () => {
+        let res = await request(server).get('/games');
+        expect(res.status).toBe(200);
+    });
+
+     it('returns an array', async () => {
+        let res = await request(server).get('/games');
+        expect(Array.isArray(res.body)).toBeTruthy();
+    });
+
+     it('returns an object array', async () => {
+        let res = await request(server).get('/games');
+        for (i = 0; i < res.body.length; i++) { 
+            expect(typeof res.body[i]).toEqual('object');
+        };
+    });
+});
+
+ /* Games POST testing */
+describe('POST ("/games")', () =>{
+    it('returns 201 with correct data', async () => {
+        const body = {
+            title: 'The most dangerous game',
+            genre: 'Bad editing',
+            releaseYear: 2133
+        };
+        let res = await request(server).post('/games').send(body);
+        expect(res.status).toBe(201);
+    });
+
+     it('returns an object', async () => {
+        const body = {
+            title: 'Pacman',
+            genre: 'Arcade',
+            releaseYear: 1980
+        };
+        let res = await request(server).post('/games').send(body);
+        expect(typeof res.body).toEqual('object');
+    });
+
+     it('returns 422 with incorrect data', async () => {
+        const body = {
+            title: 'Pacman',
+            releaseYear: 1980
+        };
+        let res = await request(server).post('/games').send(body);
+        expect(res.status).toBe(422);
+    });
+}); 
--- data/dummyDb.js
+++
@@ -1,9 +0,0 @@
-const games = [
-    {
-        title: 'Pacman', // required
-        genre: 'Arcade', // required
-        releaseYear: 1980 // not required
-    }
-];
-
- module.exports = games; 
